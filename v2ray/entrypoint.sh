c14cfeca88e4890e56e60f12aa3ca94976fd61fb
Joey <joeychen0911@yahoo.com>
2018-03-09 23:43:56 +0800

    Update entrypoint.sh

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -4,6 +4,7 @@ CMD=$1
 CONFIG=$2
 
 if [ "$CONFIG" != "" ] && [ "$CMD" == "-c" ]; then
+  echo "$CONFIG"
   echo "$CONFIG" > /etc/v2ray/config.json
   echo -e "\033[32mUse a custom configuration...\033[0m"
 fi
